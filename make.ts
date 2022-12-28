0b6165a0a3e70f3b78af8866b372cda137f64a2c
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-12-28 12:33:14 -0500

    Initial commit

---
+++ make.ts
@@ -0,0 +1,10 @@
+import {EndUserProps as BeDefEUP} from 'be-definitive/types';
+
+export const make = {
+    ":host": {
+        be: "definitive",
+        having: {
+
+        } as BeDefEUP,
+    }
+};
